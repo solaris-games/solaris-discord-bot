946231b46d3d6d8983ffada7174bfe48d8d45cb0
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-12-14 19:51:47 +0100

    Implement honeypot

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ dotenv.load_dotenv()
 
 suggestion_channel_ids = [int(id) for id in os.environ['CHAT_IDS'].split(",") if id.isdigit()]
 
+honeypot_channel_ids = [int(id) for id in os.environ['HONEYPOT_IDS'].split(",") if id.isdigit()]
+
 bot = lightbulb.BotApp(
   os.environ["DISCORD_BOT_TOKEN"],
   intents=hikari.Intents.ALL,
 )
 
-miru.install(bot)  
+miru.install(bot)
 bot.load_extensions_from("./extensions/")
-
 
 @bot.listen()
 async def on_starting(event: hikari.StartingEvent) -> None:
@@ -40,9 +41,57 @@ async def on_stopping(event: hikari.StoppingEvent) -> None:
 
 @bot.listen()
 async def on_message(event: hikari.MessageCreateEvent):
-   await auto_react(event)
-   await auto_embed(event)
+    handled1 = await check_honeypot(event)
+    if handled1:
+        True
+    await auto_react(event)
+    await auto_embed(event)
 
+
+async def check_honeypot(event: hikari.MessageCreateEvent):
+    if event.channel_id in honeypot_channel_ids:
+        app = event.app
+        author = event.author
+
+        try:
+            channel = event.get_channel()
+            if not event.guild_id:
+                return
+
+            if event.is_bot:
+                return
+
+            guild = await app.rest.fetch_guild(event.guild_id)
+            channel = await app.rest.fetch_channel(event.channel_id)
+            bot_member = guild.get_member(event.app.application.id)
+
+            roles = await bot_member.fetch_roles()
+            permissions = hikari.Permissions.NONE
+            for role in roles:
+                permissions |= role.permissions
+
+            can_ban = (permissions & hikari.Permissions.BAN_MEMBERS) == hikari.Permissions.BAN_MEMBERS
+
+            if not can_ban:
+                print("Bot is missing permissions")
+                return
+
+            await app.rest.ban_user(
+                guild=event.guild_id,
+                user=author.id,
+                reason="Rule violation",
+                delete_message_seconds=3600
+            )
+            print(f"Banned {author}")
+            return True
+
+        except hikari.ForbiddenError:
+            print("Missing permissions or role hierarchy issue")
+
+        except hikari.NotFoundError:
+            print("User already banned or not found")
+
+    return False
 
 async def auto_embed(event: hikari.MessageCreateEvent):
    # Ensure the bot doesn't react to its own messages
